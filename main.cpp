99590272e195d2222015b6b84da9adb793dfc3fa
Skobrák Erik Zsolt <skobizsolt@gmail.com>
2022-09-11 20:44:30 +0200

    feat: feature - issues has been detected

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //konstans változó neve: N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //hiányzó ;
+    for (int i = 0;) //ciklusmag hiányos (ciklusfeltétel, iterátor)
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //hiányzó dereferencia
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hiányzó ciklusfeltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzó ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) //; helyett ,
     {
-        atlag += b[i]
+        atlag += b[i] //hiányzó ; és dereferencia
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //hiányzó destruktor (delete b;)
     return 0;
 }
